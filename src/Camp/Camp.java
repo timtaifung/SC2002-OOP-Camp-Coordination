61bf55233430dde092725e197207fc524158fe52
whackcheese <128443135+whackcheese@users.noreply.github.com>
2023-11-07 02:04:23 +0800

    jess

--- src/Camp/Camp.java
+++ src/Camp/Camp.java
@@ -18,4 +18,19 @@ public class Camp {
     public ArrayList<User> getAttendanceList(){
         return this.attendanceList;
     }
+
+
 }
+
+
+
+/*create camp need to include: 
+o Camp Name
+o Dates
+o Registration closing date
+o User group this camp is open to own school or whole NTU
+o Location
+o Total Slots
+o Camp Committee Slots (max 10)
+o Description
+o Staff in charge (automatically tied to the staff who created it) */
