663c597f81fa71f2a23225472ef7b721d265d03e
Nic-Koh <99525376+Nic-Koh@users.noreply.github.com>
2023-11-12 20:24:28 +0800

    deleted campcommittee reader from excelreader

--- new_src/Assets/ExcelReader.java
+++ new_src/Assets/ExcelReader.java
@@ -10,14 +10,14 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.util.Objects;
 
-
 public class ExcelReader 
 {
     public static ArrayList<User> read(String excelFile) 
     {
         ArrayList<User> userList = new ArrayList<User>();
         try (FileInputStream fis = new FileInputStream(new File(excelFile));
-            Workbook workbook = new XSSFWorkbook(fis)) {
+            Workbook workbook = new XSSFWorkbook(fis)) 
+            {
 
             Sheet sheet = workbook.getSheetAt(0);
 
@@ -26,57 +26,39 @@ public class ExcelReader
                 Cell nameCell = row.getCell(0);
                 Cell emailCell = row.getCell(1);
                 Cell facultyCell = row.getCell(2);
-                Cell roleCell =row.getCell(3);
-                Cell isCampCommiteeCell = row.getCell(4);
+                Cell roleCell = row.getCell(3);
 
-                if (nameCell != null && emailCell != null && facultyCell !=null) 
+                if (nameCell != null && emailCell != null && facultyCell != null) 
                 {
                     String name = nameCell.toString();
                     String email = emailCell.toString();
                     String faculty = facultyCell.toString();
                     String role = roleCell.toString();
-                    String commiteeIdentifier = isCampCommiteeCell.toString();
-                    boolean isCampCommitee;
-                    if(commiteeIdentifier == "TRUE")
-                    {
-                        isCampCommitee = true;
-                    }
 
-                    else
-                    {
-                        isCampCommitee = false;
-                    }
+                    if (Objects.equals(name, "")) 
+                        return userList;
 
-                    if(Objects.equals(name, "")) 
-                        break;
-
-                    switch(role)
+                    switch (role) 
                     {
                         case "Student":
-                            if(isCampCommitee)
-                            {
-                                CampCommittee campCommittee = new CampCommittee(name,email,faculty,role);
-                                userList.add(campCommittee);
-                            }
-
-                            else
-                            {
-                                Student student = new Student(name,email,faculty,role);
-                                userList.add(student);
-                            }
+                            Student student = new Student(name, email, faculty, role);
+                            userList.add(student);
                             break;
 
                         case "Staff":
-                            Staff staff = new Staff(name,email,faculty,role);
+                            Staff staff = new Staff(name, email, faculty, role);
                             userList.add(staff);
                             break;
                     }
                 }
             }
-        } catch (IOException e) {
+        } 
+        
+        catch (IOException e) 
+        {
             e.printStackTrace();
         }
-        // Display the users stored in the ArrayList
+
         for (User user : userList) {
             System.out.println("User ID: " + user.getUserID());
         }
--- new_src/Main.java
+++ new_src/Main.java
@@ -8,8 +8,6 @@ import Menu.*;
 
 import java.util.ArrayList;
 
-
-
 public class Main {
     public static void main(String[] args){
         /* ---------------Declaration of Databases ----------------------*/
