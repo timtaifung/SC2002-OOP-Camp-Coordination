28271bb560fb854a05e9170f4b01d087b6cec381
Nic-Koh <99525376+Nic-Koh@users.noreply.github.com>
2023-11-14 18:35:22 +0800

    fixed campcomittee to work as intended

--- new_src/Assets/Roles/Student.java
+++ new_src/Assets/Roles/Student.java
@@ -8,19 +8,19 @@ public class Student extends User
 {
     private ArrayList<Camp> studentCampList;
     private ArrayList<Enquiry> studentEnquiryList;
-    private Boolean isCampCommitee;
+    private ArrayList<String> isCampCommittee;
 
     public Student(String name, String email, String faculty, String role) 
     {
         super(name, email, faculty,role);
         this.studentCampList = new ArrayList<Camp>();
         this.studentEnquiryList = new ArrayList<Enquiry>();
-        this.isCampCommitee = false;
+        this.isCampCommittee = new ArrayList<String>();
     }
 
-    public boolean getIsCampCommitee()
+    public ArrayList<String> getIsCampCommittee()
     {
-        return this.isCampCommitee;
+        return this.isCampCommittee;
     }
 
     public ArrayList<Camp> getStudentCampList()
@@ -33,8 +33,8 @@ public class Student extends User
         return this.studentEnquiryList;
     }
 
-    public void setIsCampCommitee(Boolean isCampCommitee)
+    public void setIsCampCommittee(String camp)
     {
-        this.isCampCommitee = isCampCommitee;
+        this.isCampCommittee.add(camp);
     }
 }
