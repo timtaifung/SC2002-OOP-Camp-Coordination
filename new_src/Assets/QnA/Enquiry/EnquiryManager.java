708cb0493595b5da3d8d9d7f0bc3ff4a16130c1f
Nic-Koh <99525376+Nic-Koh@users.noreply.github.com>
2023-11-25 21:28:17 +0800

    added formatting for enquirymanager

--- new_src/Assets/QnA/Enquiry/EnquiryManager.java
+++ new_src/Assets/QnA/Enquiry/EnquiryManager.java
@@ -3,6 +3,7 @@ package Assets.QnA.Enquiry;
 import Assets.Camp.Camp;
 import Assets.Roles.User;
 
+import java.text.NumberFormat.Style;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -27,54 +28,77 @@ public class EnquiryManager extends Enquiry {
 
     public static void newEnquiry(ArrayList<Enquiry> enquiryList, User currentUser, Camp currentCamp) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("-------------------------");
         System.out.println("Enter your enquiry title:");
+        System.out.println("-------------------------\n");
         String title = sc.nextLine();
+        System.out.println("---------------------------");
         System.out.println("Enter your enquiry content:");
+        System.out.println("---------------------------\n");
         String content = sc.nextLine();
         Enquiry newEnquiry = new Enquiry(currentUser);
         newEnquiry.setTitle(title);
         newEnquiry.setContent(content);
         enquiryList.add(newEnquiry);
+        System.out.println("------------------");
         System.out.println("Enquiry submitted!");
+        System.out.println("------------------\n");
     }
 
     public static void editEnquiry(Enquiry currentEnquiry, User currentUser) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("-----------------------");
         System.out.println("1. Edit enquiry title");
         System.out.println("2. Edit enquiry content");
         System.out.println("3. Exit");
+        System.out.println("-----------------------\n");
         Integer x = sc.nextInt();
         switch (x) {
-            case 1: {
+            case 1: 
+            {
+                System.out.println("-------------------------");
                 System.out.println("Enter your enquiry title:");
+                System.out.println("-------------------------\n");
                 String title = sc.nextLine();
                 sc.nextLine();
                 currentEnquiry.setTitle(title);
+                System.out.println("----------------------");
                 System.out.println("Enquiry title updated!");
+                System.out.println("----------------------\n");
                 break;
             }
 
             case 2: {
+                System.out.println("---------------------------");
                 System.out.println("Enter your enquiry content:");
+                System.out.println("---------------------------\n");
                 String content = sc.nextLine();
                 sc.nextLine();
                 currentEnquiry.setContent(content);
+                System.out.println("------------------------");
                 System.out.println("Enquiry content updated!");
+                System.out.println("------------------------\n");
                 break;
             }
 
             case 3:
+                System.out.println("----");
                 System.out.println("Exit");
+                System.out.println("----\n");
                 break;
 
             default:
+                System.out.println("--------------");
                 System.out.println("Invalid input!");
+                System.out.println("--------------\n");
                 break;
         }
     }
 
     public static void deleteEnquiry(Enquiry currentEnquiry, ArrayList<Enquiry> enquiryList) {
         enquiryList.remove(currentEnquiry);
+        System.out.println("----------------");
         System.out.println("Enquiry deleted!");
+        System.out.println("----------------\n");
     }
 }
