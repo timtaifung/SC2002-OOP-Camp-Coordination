7329dc238c32b10921cd226b83f00d99a919c1a4
Nic-Koh <99525376+Nic-Koh@users.noreply.github.com>
2023-11-25 21:44:01 +0800

    Adding more formatting

--- new_src/Assets/QnA/Suggestion/SuggestionViewer.java
+++ new_src/Assets/QnA/Suggestion/SuggestionViewer.java
@@ -4,56 +4,82 @@ import Assets.Roles.User;
 
 import java.util.ArrayList;
 
-public class SuggestionViewer extends Suggestion {
-    public SuggestionViewer(User currentUser) {
+public class SuggestionViewer extends Suggestion 
+{
+    public SuggestionViewer(User currentUser) 
+    {
         super(currentUser);
     }
 
     public static Boolean getAllSuggestion(ArrayList<Suggestion> suggestionList) {
-        if (suggestionList.isEmpty()) {
-            System.out.println("There are no suggestions currently!\n");
+        if (suggestionList.isEmpty()) 
+        {
+            System.out.println("-----------------------------------");
+            System.out.println("There are no suggestions currently!");
+            System.out.println("-----------------------------------\n");
             return false;
         }
         Integer i = 1;
-        for (Suggestion currentSuggestion : suggestionList) {
+        for (Suggestion currentSuggestion : suggestionList) 
+        {
+            System.out.println("-----------------------------------");
             System.out.println(i + ". " + currentSuggestion.getTitle());
+            System.out.println("-----------------------------------\n");
         }
         return true;
     }
 
-    public static void viewMySuggestion(ArrayList<Suggestion> suggestionList, User currentUser) {
+    public static void viewMySuggestion(ArrayList<Suggestion> suggestionList, User currentUser) 
+    {
         Integer i = 1;
-        for (Suggestion currentSuggestion : suggestionList) {
-            if (currentSuggestion.getCreatedBy().equals(currentUser)) {
+        for (Suggestion currentSuggestion : suggestionList) 
+        {
+            if (currentSuggestion.getCreatedBy().equals(currentUser)) 
+            {
+                System.out.println("-----------------------------------");
                 System.out.println(i + ". " + currentSuggestion.getTitle() + " " + currentSuggestion.getCheckedStatus());
+                System.out.println("-----------------------------------\n");
                 i++;
             }
         }
     }
 
-    public static void viewAllSuggestion(ArrayList<Suggestion> suggestionList) {
+    public static void viewAllSuggestion(ArrayList<Suggestion> suggestionList) 
+    {
         Integer i = 1;
-        for (Suggestion currentSuggestion : suggestionList) {
+        for (Suggestion currentSuggestion : suggestionList) 
+        {
+            System.out.println("-----------------------------------");
             System.out.println(i + ". " + currentSuggestion.getTitle());
+            System.out.println("-----------------------------------\n");
             i++;
         }
     }
 
     public static void viewAllPendingSuggestion(ArrayList<Suggestion> suggestionList) {
         Integer i = 1;
-        for (Suggestion currentSuggestion : suggestionList) {
-            if (!currentSuggestion.getApprovedStatus()) {
+        for (Suggestion currentSuggestion : suggestionList) 
+        {
+            if (!currentSuggestion.getApprovedStatus()) 
+            {
+                System.out.println("-----------------------------------");
                 System.out.println(i + ". " + currentSuggestion.getTitle());
+                System.out.println("-----------------------------------\n");
                 i++;
             }
         }
     }
 
-    public static void viewAllApprovedSuggestion(ArrayList<Suggestion> suggestionList) {
+    public static void viewAllApprovedSuggestion(ArrayList<Suggestion> suggestionList) 
+    {
         Integer i = 1;
-        for (Suggestion currentSuggestion : suggestionList) {
-            if (currentSuggestion.getApprovedStatus()) {
+        for (Suggestion currentSuggestion : suggestionList) 
+        {
+            if (currentSuggestion.getApprovedStatus()) 
+            {
+                System.out.println("-----------------------------------");
                 System.out.println(i + ". " + currentSuggestion.getTitle());
+                System.out.println("-----------------------------------\n");
                 i++;
             }
         }
