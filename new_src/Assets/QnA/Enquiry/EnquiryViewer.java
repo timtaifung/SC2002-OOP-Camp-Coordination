15cfb7b149ac62e12f8fc2b77735b4098c92a98a
Nic-Koh <99525376+Nic-Koh@users.noreply.github.com>
2023-11-25 21:41:17 +0800

    Added more formatting

--- new_src/Assets/QnA/Enquiry/EnquiryViewer.java
+++ new_src/Assets/QnA/Enquiry/EnquiryViewer.java
@@ -12,12 +12,18 @@ public class EnquiryViewer extends Enquiry {
     public static Boolean getAllEnquiry(ArrayList<Enquiry> enquiryList) {
         if (enquiryList.isEmpty()) 
         {
-            System.out.println("There are no enquiries currently!\n");
+            System.out.println("---------------------------------");
+            System.out.println("There are no enquiries currently!");
+            System.out.println("---------------------------------\n");
             return false;
         }
+
         Integer i = 1;
-        for (Enquiry currentEnquiry : enquiryList) {
+        for (Enquiry currentEnquiry : enquiryList) 
+        {
+            System.out.println("---------------------------------");
             System.out.println(i + ". " + currentEnquiry.getTitle());
+            System.out.println("---------------------------------\n");
             i++;
         }
         return true;
@@ -25,14 +31,22 @@ public class EnquiryViewer extends Enquiry {
 
     public static void viewMyEnquiry(ArrayList<Enquiry> enquiryList, User currentUser) {
         Integer i = 1;
-        for (Enquiry currentEnquiry : enquiryList) {
-            if (currentEnquiry.getCreatedBy().equals(currentUser)) {
+        for (Enquiry currentEnquiry : enquiryList) 
+        {
+            if (currentEnquiry.getCreatedBy().equals(currentUser)) 
+            {
+                System.out.println("---------------------------------");
                 System.out.print(i + ". " + currentEnquiry.getTitle() + "     ");
-                if (currentEnquiry.getProcessedStatus()) {
+                if (currentEnquiry.getProcessedStatus()) 
+                {
                     System.out.println("Processed");
                     System.out.println("Reply: " + currentEnquiry.getreply());
-                } else {
+                    System.out.println("---------------------------------\n");
+                }
+                else 
+                {
                     System.out.println("Not Processed");
+                    System.out.println("---------------------------------\n");
                 }
                 i++;
             }
