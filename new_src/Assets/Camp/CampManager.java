5578f3838b9025c2351d50d4b8e028623cbbfeca
Nic-Koh <99525376+Nic-Koh@users.noreply.github.com>
2023-11-25 22:24:04 +0800

    fixed spelling error

--- new_src/Assets/Camp/CampManager.java
+++ new_src/Assets/Camp/CampManager.java
@@ -274,14 +274,16 @@ public class CampManager {
 
     public static void campDetail(Camp currentCamp) 
     {
+        System.out.println("--------------------------------------------");
         System.out.println("Camp Name: " + currentCamp.getName());
         System.out.println("Date of Camp: " + currentCamp.getDateofCamp());
-        System.out.println("Registeration Closing Date: " + currentCamp.getRegisteraionClosing());
+        System.out.println("Registeration Closing Date: " + currentCamp.getRegistrationClosing());
         System.out.println("Faculty/School: " + currentCamp.getGrouping());
         System.out.println("Location: " + currentCamp.getLocation());
         System.out.println("Total Slots: " + currentCamp.getTotalSlots());
         System.out.println("Short Description: " + currentCamp.getShortDescription());
         System.out.println("Available Slots: " + currentCamp.getAvailableSlots());
+        System.out.println("--------------------------------------------");
     }
 
     public static void applyCampCommitee(Student currentStudent, Camp currentCamp, ArrayList<CampCommittee> campCommitteeList) {
