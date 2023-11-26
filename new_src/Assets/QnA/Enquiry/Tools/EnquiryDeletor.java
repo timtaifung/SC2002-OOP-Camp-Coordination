8af40cbde8b28cbfd62f51dd1c3770adb06023d4
Timo <nyantimothy@gmail.com>
2023-11-26 11:32:28 +0800

    cannot delete after processed for Suggestion and EnquiryDeletor

--- new_src/Assets/QnA/Enquiry/Tools/EnquiryDeletor.java
+++ new_src/Assets/QnA/Enquiry/Tools/EnquiryDeletor.java
@@ -6,6 +6,12 @@ import java.util.ArrayList;
 
 public class EnquiryDeletor {
     public static void deleteEnquiry(Enquiry currentEnquiry, ArrayList<Enquiry> enquiryList) {
+        if(currentEnquiry.getProcessedStatus()){
+            System.out.println("------------------------------------------------");
+            System.out.println("Enquiry has been processed and cannot be deleted!");
+            System.out.println("------------------------------------------------\n");
+            return;
+        }
         enquiryList.remove(currentEnquiry);
         System.out.println("----------------");
         System.out.println("Enquiry deleted!");
--- new_src/Assets/QnA/Suggestion/Tools/SuggestionDeletor.java
+++ new_src/Assets/QnA/Suggestion/Tools/SuggestionDeletor.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 public class SuggestionDeletor {
     public static void deleteSuggestion(Suggestion currentSuggestion, ArrayList<Suggestion> suggestionList)
     {
+        if(currentSuggestion.getCheckedStatus()){
+            System.out.println("------------------------------------------------");
+            System.out.println("Suggestion has been processed and cannot be deleted!");
+            System.out.println("------------------------------------------------\n");
+            return;
+        }
         suggestionList.remove(currentSuggestion);
         System.out.println("-------------------");
         System.out.println("Suggestion deleted!");
